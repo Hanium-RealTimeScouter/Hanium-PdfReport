2554db2dd88d3c2da971ea2e1d30767748e876d8
S.J. Lee <occidere@naver.com>
2017-07-30 18:23:37 +0900

    Main.java was deprecated

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import scouter.plugin.server.hanium.pdfreport.email.PdfEmail;
 
 public class Main {
-
+	
+	/* Main.java is just test class for local environment */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PdfEmail p = new PdfEmail();
